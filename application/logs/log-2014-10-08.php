f320cd020c5893ed049308883c2deea994d21b3b
jorge <jtorrescolan@gmail.com>
2014-10-08 08:50:43 -0500

    OK :D

---
+++ application/logs/log-2014-10-08.php
@@ -0,0 +1,4 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2014-10-08 08:46:29 --> Severity: Notice  --> Undefined index: is_logued_in C:\wamp\www\appclinica\application\controllers\login\login.php 11
+ERROR - 2014-10-08 08:46:35 --> Severity: Notice  --> Undefined index: is_logued_in C:\wamp\www\appclinica\application\controllers\login\login.php 11
